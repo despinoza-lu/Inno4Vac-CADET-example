1ee56b7f87339d5326c27cf72d75e92742fb747e
despinaca <daniel.espinoza@chemeng.lth.se>
2024-12-17 08:36:50 +0100

    Check out CADET Python Simulator from Git

--- code/CADET-Python-Simulator-master/CADETPythonSimulator/distribution_base.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-class DistributionBase:
-    """Small wrapper class for Concentration."""
-
-    def get_distribution(t: float, section_nr: int)->np.ndarray:
-        """Abtract class to call."""
-        pass
