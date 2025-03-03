ebd4bdeada63e7af786695562fe041585ae04e3d
djpespinoza <djp.espinoza@gmail.com>
2025-03-03 11:39:06 +0100

    - Tested Hagen-Poiseuille-based filter resistance. - Fixed some clarity in the plots.

--- code/cell_lysate_filtration.py
+++ code/cell_lysate_filtration.py
@@ -49,15 +49,15 @@ filter_height = np.sqrt(filter_area/np.pi)*2*3  # m, assuming 1:3 aspect ratio t
 r_pore = (.45 + .2)/2 * 1e-6  # m
 eps_filter = .8  # Filter porosity
 dyn_visc = 1.05e-3  # P.s
-# R_m = 8*dyn_visc*filter_height/eps_filter/r_pore**2
+R_m = 8*dyn_visc*filter_height/eps_filter/r_pore**2
 C = 1
 permeability = C*r_pore**2
-R_m = filter_height/permeability
-filter_resistance = 1
+# R_m = filter_height/permeability
+filter_resistance = R_m#1
 max_operating_pressure = 5e5  # Pa
 
-permeate_tank_volume = 1e-9  # liters, intial volume in permeate tank
-flowrate_into_DEF = 1  # m^3/s
+permeate_tank_volume = 1e-9  # m^3, intial volume in permeate tank
+flowrate_into_DEF = 1*.001/3600  # m^3/s
 flowrate_out_of_permeate_tank = 1e-9  # m^3/s
 
 if case == 1:
@@ -70,18 +70,23 @@ distr = np.array(data['mass fractions'])
 
 MWs = (d*1e-9/2)**2*np.pi*rho_debris/avogadro*1000
 
-MW_protein = 82358.0
+MW_protein = 82358.0  # g/mol
+MW_water = 18  # g/mol
 
 MWs = MW_protein*np.array([1500, 1000, 1])
+cake_resistance = 1
 # MWs = np.append(MWs)
 
-# biomass_per_liter = 379  # g/l
-# mass_of_fermentation_broth = 27819  # ml
-# total_biomass = biomass_per_liter * (mass_of_fermentation_broth*1e-3)  # g
-# broth_fraction = (mass_of_fermentation_broth-total_biomass)/mass_of_fermentation_broth
-# component_masses = total_biomass*distr
-# component_distr = component_masses / mass_of_fermentation_broth
-# distr = np.append(component_distr, broth_fraction)
+biomass_per_liter = 379  # g/l
+mass_of_fermentation_broth = 27819  # ml
+total_biomass = biomass_per_liter * (mass_of_fermentation_broth*1e-3)  # g
+broth_fraction = (mass_of_fermentation_broth-total_biomass)/mass_of_fermentation_broth
+component_masses = total_biomass*distr
+component_distr = component_masses / mass_of_fermentation_broth
+distr = np.append(component_distr, broth_fraction)
+
+volume_to_filter = mass_of_fermentation_broth*1e-6  # m^3
+time_to_filter = volume_to_filter/flowrate_into_DEF  # s
 
 # %%
 permvol = []
@@ -93,15 +98,15 @@ tankvol = []
 tankcon = []
 
 component_system = CPSComponentSystem(
-                    name="cell_debris",
-                    components=len(MWs),
-                    pure_densities=[rho_debris]*len(MWs),
-                    molecular_weights=MWs,
-                    viscosities=[1]*len(MWs),
-                    specific_cake_resistances=[1]*len(MWs)
+                    name="cell_lysate",
+                    components=len(MWs) + 1,
+                    pure_densities=[rho_debris]*len(MWs) + [rho_water],
+                    molecular_weights=list(MWs) + [MW_water],
+                    viscosities=[dyn_visc]*(len(MWs) + 1),
+                    specific_cake_resistances=[cake_resistance]*len(MWs) + [0],
                     )
 
-rejectionmodell = StepCutOff(cutoff_weight=MWs.min()*1.1)
+rejectionmodell = StepCutOff(cutoff_weight=MW_protein*1.1)
 vol_dist = distribution()
 vol_dist.set_distr = distr
 inlet = DistributionInlet(component_system=component_system, name="inlet")
@@ -121,7 +126,7 @@ system = FlowSystem(unit_operations=unit_operation_list)
 section = [
             {
                 'start': 0,
-                'end': 110,
+                'end': time_to_filter,
                 'connections': [
                     [0, 1, 0, 0, flowrate_into_DEF],
                     [1, 2, 0, 0, flowrate_out_of_permeate_tank],
@@ -131,7 +136,7 @@ section = [
 
 system.initialize_state()
 system.states['deadendfilter']['permeate_tank']['tankvolume'] = permeate_tank_volume
-system.states['deadendfilter']['permeate_tank']['c'] = np.array([0, 0, 0])
+system.states['deadendfilter']['permeate_tank']['c'] = np.array([0, 0, 0, 0])
 solver = Solver(system, section)
 
 solver.solve()
@@ -196,21 +201,31 @@ fig.suptitle(f'Tank volume')
 axes.title.set_text(f'{title}% Retention')
 
 axes.set_xlabel('$t$ [$s$]')
-axes.set_ylabel('$V^T$ [$m^3$]')
+axes.set_ylabel('$V^T$ [$liters$]')
 
-axes.plot(t, tankvol[i][:, 0], 'o', label='$V^T$')
+axes.plot(t, tankvol[i][:, 0]*1e3, 'o', label='$V^T$')
 # axes.set_box_aspect(1)
 axes.legend()
 # axes.set_xticks(t[0::2])
 fig.tight_layout()
 
+alpha_value = .3
+
 fig_c, ax_c = plt.subplots(2, 1, figsize=(6.4, 2*4.8))
-lines = ax_c[0].plot(t, tankconcentrations, alpha=.3)
+lines = ax_c[0].plot(t, tankconcentrations[:, :-1], alpha=alpha_value)
+ax_c_0_twin = ax_c[0].twinx()
+water_line = ax_c_0_twin.plot(t, tankconcentrations[:, -1], alpha=1, color='orange')
 ax_c[0].set_title('Concentration in permeate tank')
-ax_c[1].plot(t, solver.unit_solutions['deadendfilter']['cake']['c']['values'], alpha=.3)
+ax_c[1].plot(t, solver.unit_solutions['deadendfilter']['cake']['c']['values'][:, :-1], alpha=alpha_value)
+ax_c_0_twin.set_ylabel('Water', color='orange')
+ax_c_0_twin.set_ylim(top=60)
+ax_c_1_twin = ax_c[1].twinx()
+ax_c_1_twin.plot(t, solver.unit_solutions['deadendfilter']['cake']['c']['values'][:, -1], alpha=1, color='orange')
 ax_c[1].set_title('Concentration in filter cake')
-labels = ['Debris 1', 'Debris 2', 'Proteins']
-fig_c.legend(lines, labels, loc='outside lower center', ncols=3, frameon=False)
+ax_c_1_twin.set_ylabel('Water', color='orange')
+ax_c_1_twin.set_ylim(top=35)
+labels = ['Debris 1', 'Debris 2', 'Proteins', 'Water']
+fig_c.legend(lines + water_line, labels, loc='outside lower center', ncols=2, frameon=False)
 fig_c.tight_layout()
 fig_c.subplots_adjust(bottom=.1)
 
